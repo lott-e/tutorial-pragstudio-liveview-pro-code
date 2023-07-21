9f1b99decbfc7507fd18c5bc02033dbfc59bee15
lott-e <hi.lottie@icloud.com>
2023-07-21 20:02:15 +0100

    first commit

---
+++ live_view_studio/lib/live_view_studio_web/live/light_live.ex
@@ -0,0 +1,13 @@
+defmodule LiveViewStudioWeb.LightLive do
+  use LiveViewStudioWeb, :live_view
+
+  #mount, first callback invoked. Auto called when request comes in through router
+  def mount() do
+
+  end
+
+  #render
+
+  #handle_event
+
+end
