62f52154849173bce7087a2db5edc876940e3f54
lott-e <hi.lottie@icloud.com>
2023-07-22 19:52:50 +0100

    boats

---
+++ live_view_studio/lib/live_view_studio_web/components/custom_components.ex
@@ -0,0 +1,23 @@
+defmodule LiveViewStudioWeb.CustomComponents do
+  use Phoenix.Component
+
+  attr(:expiration, :integer, default: 24)
+  slot(:inner_block, required: true)
+  slot(:legal)
+
+  def promo(assigns) do
+    ~H"""
+      <div class="promo">
+       <div class="deal">
+       <%= render_slot(@inner_block) %>
+       </div>
+       <div class="expiration">
+        Deal expires in <%= @expiration %> Hours
+       </div>
+       <div class="legal">
+        <%= render_slot(@legal) %>
+       </div>
+    </div>
+    """
+  end
+end
--- live_view_studio/lib/live_view_studio_web/live/boats_live.ex
+++ live_view_studio/lib/live_view_studio_web/live/boats_live.ex
@@ -1,7 +1,7 @@
 defmodule LiveViewStudioWeb.BoatsLive do
   use LiveViewStudioWeb, :live_view
-
   alias LiveViewStudio.Boats
+  import LiveViewStudioWeb.CustomComponents
 
   def mount(_params, _session, socket) do
     socket =
@@ -10,14 +10,60 @@ defmodule LiveViewStudioWeb.BoatsLive do
         boats: Boats.list_boats()
       )
 
-    {:ok, socket}
+    # temporary_assigns saves memory for static data
+    {:ok, socket, temporary_assigns: [boats: []]}
   end
 
   def render(assigns) do
     ~H"""
     <h1>Daily Boat Rentals</h1>
+    <.promo>
+      Get your new best friend today!
+      <:legal>
+        <Heroicons.exclamation_circle />
+         Friend is a boat
+       </:legal>
+    </.promo>
     <div id="boats">
-      <form>
+    <%!-- every time filter changes this component auto re renders --%>
+    <.filter_form filter={@filter} />
+
+    <div class="boats">
+      <.boat :for={boat <- @boats} boat={boat}/>
+    </div>
+
+    <%!-- <.boat_list boats={@boats} /> --%>
+       <.promo expiration={4}>
+         Save 25% on rentals!
+         <:legal>
+         <Heroicons.exclamation_circle />
+          Excluding weekends
+         </:legal>
+       </.promo>
+    </div>
+    """
+  end
+
+  def handle_event("filter", %{"type" => type, "prices" => prices}, socket) do
+    filter = %{type: type, prices: prices}
+    boats = Boats.list_boats(filter)
+    {:noreply, assign(socket, boats: boats, filter: filter)}
+  end
+
+  defp type_options do
+    [
+      "All Types": "",
+      Fishing: "fishing",
+      Sporting: "sporting",
+      Sailing: "sailing"
+    ]
+  end
+
+  attr(:filter, :map, required: true)
+
+  def filter_form(assigns) do
+    ~H"""
+     <form phx-change="filter">
         <div class="filters">
           <select name="type">
             <%= Phoenix.HTML.Form.options_for_select(
@@ -40,34 +86,29 @@ defmodule LiveViewStudioWeb.BoatsLive do
           </div>
         </div>
       </form>
-      <div class="boats">
-        <div :for={boat <- @boats} class="boat">
-          <img src={boat.image} />
-          <div class="content">
-            <div class="model">
-              <%= boat.model %>
-            </div>
-            <div class="details">
-              <span class="price">
-                <%= boat.price %>
-              </span>
-              <span class="type">
-                <%= boat.type %>
-              </span>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
     """
   end
 
-  defp type_options do
-    [
-      "All Types": "",
-      Fishing: "fishing",
-      Sporting: "sporting",
-      Sailing: "sailing"
-    ]
+  attr(:boat, LiveViewStudio.Boats.Boat, required: true)
+
+  def boat(assigns) do
+    ~H"""
+        <div class="boat">
+          <img src={@boat.image} />
+          <div class="content">
+            <div class="model">
+              <%= @boat.model %>
+            </div>
+            <div class="details">
+              <span class="price">
+                <%= @boat.price %>
+              </span>
+              <span class="type">
+                <%= @boat.type %>
+              </span>
+            </div>
+          </div>
+      </div>
+    """
   end
 end
